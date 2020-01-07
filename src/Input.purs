258af633de79e3a7833a9940f5517d66f22c2a71
Alexandre Gigliotti <alexandre.f.gigliotti@gmail.com>
2020-01-06 18:08:59 -0800

    Display users full name.

--- src/Input.purs
+++ src/Input.purs
@@ -15,7 +15,7 @@ import Data.Argonaut.Parser as J
 import Data.Either (hush)
 import Data.Maybe (Maybe(..))
 import Effect.Aff.Class (class MonadAff)
-import Foreign.Object (Object)
+import Foreign.Object as Object
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
@@ -32,7 +32,7 @@ type Slot = H.Slot Query Unit
 type State =
     { username :: String
     , loading :: Boolean
-    , response :: Maybe (Object J.Json)
+    , response :: Maybe String
     }
 
 
@@ -71,7 +71,7 @@ render state =
             [ HH.text "Fetch"
             ]
         , HH.text $ show state.loading
-        , HH.div_ [ HH.text $ show $ (J.fromObject >>> J.stringify) <$> state.response ]
+        , HH.div_ [ HH.text $ show $ state.response ]
         ]
 
 
@@ -80,10 +80,13 @@ handleAction action =
     case action of
         ChangedUsername value ->
             H.modify_ \state -> state { username = value }
-        FetchUser -> do
-        --TODO What tells us that we are operating on the Halogen Monad in this do block?
-            username <- H.gets _.username
-            H.modify_ (_ {loading = true})
-            response <- H.liftAff $ AX.get AXRF.json ("https://api.github.com/users/" <> username)
-            --TODO how do I access the response body properties? (i.e. show body.name)
-            H.modify_ (_ {loading = false, response = J.toObject =<< _.body <$> (hush response) })
+        FetchUser ->
+            do --TODO What tells us that we are operating on the Halogen Monad in this do block?
+                username <- H.gets _.username
+                H.modify_ (_ {loading = true})
+                response <- H.liftAff $ AX.get AXRF.json ("https://api.github.com/users/" <> username)
+                H.modify_ (_ {loading = false, response =  do
+                    body <- J.toObject =<< _.body <$> (hush response)
+                    name <- J.toString =<< Object.lookup "name" body
+                    pure name
+                })
